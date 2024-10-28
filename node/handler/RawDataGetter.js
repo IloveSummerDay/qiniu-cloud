eb8c10c7274cf18349287e7b345797892b952a7b
Fayche <1751162157@qq.com>
2024-10-28 23:20:19 +0800

    refactor: Optimize code structure.

--- node/handler/RawDataGetter.js
+++ node/handler/RawDataGetter.js
@@ -1,48 +1,54 @@
 import { Octokit } from "octokit";
 
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
 export class RawDataGetter {
     constructor(options) {
         this.options = {
-            auth: "",
-        }
+            auth: GITHUB_TOKEN,
+        };
 
-        Object.assign(this.options, options)
-        this.octokit = new Octokit({ auth: this.options.auth })
+        Object.assign(this.options, options);
+        this.octokit = new Octokit({ auth: this.options.auth });
         this.headers = {
-            'X-GitHub-Api-Version': '2022-11-28',
-            'accept': "application/vnd.github+json"
-        }
+            "X-GitHub-Api-Version": "2022-11-28",
+            accept: "application/vnd.github+json",
+        };
     }
 
     /**
      * @param {string} owner - 仓库所有者的用户名。
      * @param {string} repo - 仓库的名称。
      * @param {string} url - GitHub API URL - stars | forks | subscribers | pulls。
-     * @param {object} other_args - 其他查询参数
+     * @param {object} otherArgs - 其他查询参数
      * @return {Promise} 包含请求结果的Promise对象。
      */
-    async get_repo_basis_info(owner, repo, url, other_args) {
-
+    async getRepoBasisInfo(owner, repo, url, otherArgs = {}) {
         return await this.octokit.request(url, {
             headers: this.headers,
             owner,
             repo,
-            ...other_args,
-        })
+            ...otherArgs,
+        });
     }
 
     /**
      * @param {string} username - 仓库所有者的用户名。
      * @param {string} url - GitHub API URL - followers | following | users
-     * @param {object} other_args - 其他查询参数
+     * @param {object} otherArgs - 其他查询参数
      * @return {Promise} 包含请求结果的Promise对象。
      */
-    async get_user_basis_info(username, url, other_args) {
+    async getUserBasisInfo(username, url, otherArgs = {}) {
         return await this.octokit.request(url, {
             headers: this.headers,
             username,
-            ...other_args,
-        })
+            ...otherArgs,
+        });
     }
 
+    async getUserViaCriteria(url) {
+        return await this.octokit.request(url, {
+            headers: this.headers,
+        });
+    }
 }
